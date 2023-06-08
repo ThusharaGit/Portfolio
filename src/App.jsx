99c88f9cea04471ff5cc3eb9146fd098ae902979
ThusharaGit <thushara.weeraparakrama@gmail.com>
2023-06-08 19:39:14 +0200

    Start portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState,useNavigate,useEffect } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
