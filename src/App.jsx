5dd90770a165af62eed68fbf1e6306cb45e33169
ThusharaGit <thushara.weeraparakrama@gmail.com>
2023-06-09 12:22:59 +0200

    remove media queries and restart with desktop version

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState,useNavigate,useEffect } from 'react'
+import data from './assets/data.json'
+import Nav from './components/Nav'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='app'>
+      <Nav/>
+      <div className='mainDiv'>
+      <h1 className='welcome'>Hello<span className='exclamation'>!</span></h1>
+      <h2 className='intro'>I'm <span className='fullName'>{data[1].fullName}</span> </h2>
+      <h1 className='position'>Frontend Developer</h1>
+      <p>{data[2].firstPageInfo}</p>
+      </div>
       
-    </>
+
+
+
+    </div>
   )
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <a href="">Home</a>
+        <a href="">About me</a>
+        <a href="">Services</a>
+        <a href="">Projects</a>
+    </div>
+  )
+}
+
+export default Nav
